48f62f7398bf272a4dc38380d6d176d4cdbbd5a0
hkhong0813 <hkhong0813@gmail.com>
2021-12-13 09:31:23 +0900

    first commit

---
+++ build/urdf_exam/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src/urdf_exam
+# Build directory: /home/ubuntu/catkin_ws/build/urdf_exam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
